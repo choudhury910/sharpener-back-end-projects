4b01d2062dc529023ea3323c09cd570ae2033a8f
unknown <reshabcsb15gmail.com.com>
2022-05-05 11:09:08 +0530

    ok

---
+++ project 1/dom.js
@@ -0,0 +1,25 @@
+// console.log(window);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.forms);
+// console.log(document.images);
+// console.log(document.title);
+// document.title='reshab';
+// console.log(document.title);
+// let headerTitle=document.getElementById('header-title');
+// let header=document.getElementById('main-header');
+// console.log(headerTitle);
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML='<h3>How are You</h3>'
+// header.style.borderBottom='solid 3px #000';
+// console.log(headerTitle);
+// headerTitle.innerText='hello';
+// let header1=document.getElementById('main');
+// console.log(header1);
+// header1.style.fontWeight='normal';
+
+
+
